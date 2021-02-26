020ca0c95f65a2c6610f230bf56ae569ed4bac8b
fmnijk <52465927+fmnijk@users.noreply.github.com>
2021-02-27 00:59:22 +0800

    Add files via upload

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.downloads.onDeterminingFilename.addListener((item, suggest) => {
+    var url_str = item.url
+    if (url_str.match('https://d2.wnacg.download/down/*')) {
+        var url = new URL(url_str);
+        newName = url.searchParams.get("n") + '.zip';
+        suggest({filename: newName, conflictAction: 'overwrite' });
+    }
+});
